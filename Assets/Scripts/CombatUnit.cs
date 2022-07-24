a7982cbc1a2d96dc6e4a106b414458837b0c1c2f
Andrii Dubovyk <and.dubovyk@gmail.com>
2022-07-24 18:26:50 +0300

    Add CombatUnit script

---
+++ Assets/Scripts/CombatUnit.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatUnit : MonoBehaviour
+{
+    public int maxHP;
+    public int attack;
+    int currentHP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHP = maxHP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Attack(CombatUnit another)
+    {
+        another.currentHP -= this.attack;
+    }
+}
